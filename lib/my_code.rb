136676ec5a728ca9e95336f2e4583872132951d6
Christopher Leja <christopher.leja@gmail.com>
2020-02-08 21:26:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,15 +50,16 @@ end
 
 def reduce_to_all_true(source_array)
   i = 0
+  
   while i < source_array.length
     if source_array[i]
-      true
+      result = true
     else
-      nil
+      result = false
     end
   i += 1
 end
-true
+result
 end
 
 def reduce_to_any_true(source_array)
